d10022d735e557f070016e8a78d09f3fde89db65
Govardhan252033 <govardhan.pinnelli@gmail.com>
2025-12-09 13:33:11 -0800

    code changes done to feature-101 by vivek

--- application.java
+++ application.java
@@ -10,3 +10,19 @@ class Main {
     System.out.println(first + " + " + second + " = "  + sum);
   }
 }
+
+# code changes for the feature-101 by vivek
+
+public class MultiplyTwoNumbers {
+
+    public static void main(String[] args) {
+
+        float first = 1.5f;
+        float second = 2.0f;
+
+        float product = first * second;
+
+        System.out.println("The product is: " + product);
+    }
+}
+# end of the code feature-101
